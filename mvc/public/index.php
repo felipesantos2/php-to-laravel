4f36e1f8ed3932058fbe964b9ae80ad7244d5ba0
Felipe Pinheiro <santospinheiro627@gmail.com>
2024-07-11 17:30:33 -0300

    install dot env

--- mvc/public/index.php
+++ mvc/public/index.php
@@ -1,2 +1,16 @@
 <?php
-// silence is golden
+
+// constantes utilitárias
+define("APP_START", microtime(true));
+define("APP_NAME", "MY MVC");
+define("APP_VERSION", "0.0.1");
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 2));
+
+$dotenv->load();
+
+// dd($_ENV["DB_HOST"]);
+
+dd($_SERVER);
